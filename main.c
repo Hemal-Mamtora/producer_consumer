09b361bd2660ddeb8cd85bcc69ca2a4aca642f29
Hemal-Mamtora <hemalmamtora1@gmail.com>
2022-02-22 14:09:13 -0600

    renamed to requestQ

--- main.c
+++ main.c
@@ -4,106 +4,116 @@
 #include <stdlib.h> // https://stackoverflow.com/questions/822323/how-to-generate-a-random-int-in-c
 #include <unistd.h> // https://pubs.opengroup.org/onlinepubs/009696799/functions/sleep.html
 #include <math.h>
+#include <time.h> // https://stackoverflow.com/questions/11765301/how-do-i-get-the-unix-timestamp-in-c-as-an-int
 
 #define SIZE 10
 #define PRODUCER_COUNT 10
 #define CONSUMER_COUNT 10
-#define TOTAL_ITEMS 20
+#define TOTAL_requestQ 20
 
 sem_t mutex, empty, full;
 
+typedef struct Request{
+  time_t time;
+}Request;
+
+typedef struct Response{
+  int ack; // boolean to store acknowledgement
+  int resource;
+}Response;
+
 // Circular Queue implementation in C
 // https://www.programiz.com/dsa/circular-queue
-
-int items[SIZE];
-int front = -1, rear = -1;
+Request requestQ[SIZE];
+int requestQFront = -1;
+int requestQRear = -1;
 
 // Check if the queue is full
-int isFull() {
-  if ((front == rear + 1) || (front == 0 && rear == SIZE - 1)) return 1;
+int requestQIsFull() {
+  if ((requestQFront == requestQRear + 1) || (requestQFront == 0 && requestQRear == SIZE - 1)) return 1;
   return 0;
 }
 
 // Check if the queue is empty
-int isEmpty() {
-  if (front == -1) return 1;
+int requestQIsEmpty() {
+  if (requestQFront == -1) return 1;
   return 0;
 }
 
 // Adding an element
-void enQueue(int element) {
-  if (isFull())
+void requestQInsert(int element) {
+  if (requestQIsFull())
     printf("\n Queue is full!! \n");
   else {
-    if (front == -1) front = 0;
-    rear = (rear + 1) % SIZE;
-    items[rear] = element;
+    if (requestQFront == -1) requestQFront = 0;
+    requestQRear = (requestQRear + 1) % SIZE;
+    requestQ[requestQRear] = element;
     //printf("\n Inserted -> %d \n", element);
   }
 }
 
 // Removing an element
-int deQueue() {
+int requestQDelete() {
   int element;
-  if (isEmpty()) {
+  if (requestQIsEmpty()) {
     printf("\n Queue is empty !! \n");
     return (-1);
   } else {
-    element = items[front];
-    if (front == rear) {
-      front = -1;
-      rear = -1;
+    element = requestQ[requestQFront];
+    if (requestQFront == requestQRear) {
+      requestQFront = -1;
+      requestQRear = -1;
     } 
     // Q has only one element, so we reset the 
     // queue after dequeing it. ?
     else {
-      front = (front + 1) % SIZE;
+      requestQFront = (requestQFront + 1) % SIZE;
     }
     // printf("\n Deleted element -> %d \n", element);
     return (element);
   }
 }
 
-// Display the queue
-void display() {
+// requestQDisplay the queue
+void requestQDisplay() {
   int i;
-  if (isEmpty())
+  if (requestQIsEmpty())
     printf(" \n Empty Queue\n");
   else {
-    printf("\n Front -> %d ", front);
-    printf("\n Items -> ");
-    for (i = front; i != rear; i = (i + 1) % SIZE) {
-      printf("%d ", items[i]);
+    printf("\n requestQFront -> %d ", requestQFront);
+    printf("\n requestQ -> ");
+    for (i = requestQFront; i != requestQRear; i = (i + 1) % SIZE) {
+      printf("%d ", requestQ[i]);
     }
-    printf("%d ", items[i]);
-    printf("\n Rear -> %d \n", rear);
+    printf("%d ", requestQ[i]);
+    printf("\n requestQRear -> %d \n", requestQRear);
   }
 }
 
 int testQeue() {
-  // Fails because front = -1
-  deQueue();
+  // Fails because requestQFront = -1
+  requestQDelete();
 
-  enQueue(1);
-  enQueue(2);
-  enQueue(3);
-  enQueue(4);
-  enQueue(5);
+  requestQInsert(1);
+  requestQInsert(2);
+  requestQInsert(3);
+  requestQInsert(4);
+  requestQInsert(5);
 
-  // Fails to enqueue because front == 0 && rear == SIZE - 1
-  enQueue(6);
+  // Fails to requestQInsert because requestQFront == 0 && requestQRear == SIZE - 1
+  requestQInsert(6);
 
-  display();
-  deQueue();
+  requestQDisplay();
+  requestQDelete();
 
-  display();
+  requestQDisplay();
 
-  enQueue(7);
-  display();
+  requestQInsert(7);
+  requestQDisplay();
 
-  // Fails to enqueue because front == rear + 1
-  enQueue(8);
-  display();
+  // Fails to requestQInsert because requestQFront == requestQRear + 1
+  requestQInsert(8);
+  requestQDisplay();
 
   return 0;
 }
@@ -130,7 +140,7 @@ int testSemaphores(){
 void* producerStartRoutine(void *arg){
   int resource;
   useconds_t sleep_time;
-  for(int i = 0; i < TOTAL_ITEMS; i++){
+  for(int i = 0; i < TOTAL_requestQ; i++){
     resource = rand() % 100;
 
     // TODO: explain in comments, how sleeping implements aging and avoids starvation
@@ -140,7 +150,7 @@ void* producerStartRoutine(void *arg){
 
     sem_wait(&empty);
     sem_wait(&mutex);
-    enQueue(resource);
+    requestQInsert(resource);
     printf("Producer %d inserted item %d in queue.\n", *(int*)arg, resource); // TODO: print Q ?
     sem_post(&mutex);
     sem_post(&full);
@@ -151,7 +161,7 @@ void* consumerStartRoutine(void *arg){
   int resource;
   useconds_t sleep_time;
 
-  for(int i = 0; i < TOTAL_ITEMS; i++){
+  for(int i = 0; i < TOTAL_requestQ; i++){
 
     // TODO: explain in comments, how sleeping implements aging and avoids starvation
     sleep_time = (useconds_t) pow(2, i % 10); // + (useconds_t) (rand() % 500);
@@ -160,7 +170,7 @@ void* consumerStartRoutine(void *arg){
 
     sem_wait(&full);
     sem_wait(&mutex);
-    resource = deQueue();
+    resource = requestQDelete();
     printf("Consumer %d consumed item %d from queue\n", *(int*)arg, resource); // TODO: print Q ?
     sem_post(&mutex);
     sem_post(&empty);
